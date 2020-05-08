5d94418565695dc513703c68333dfb6b0fc54624
Paul Razvan Berg <hello@paulrberg.com>
2020-05-08 22:51:31 +0300

    refactor(react-app): remove service worker

--- packages/react-app/src/index.js
+++ packages/react-app/src/index.js
@@ -4,9 +4,9 @@ import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 // You should replace this uri with your own and put it into a .env file
+// See all subgraphs: https://thegraph.com/explorer/
 const client = new ApolloClient({
   uri: "https://api.thegraph.com/subgraphs/name/paulrberg/create-eth-app",
 });
@@ -17,8 +17,3 @@ ReactDOM.render(
   </ApolloProvider>,
   document.getElementById("root"),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
